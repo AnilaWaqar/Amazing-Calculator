ca8afab3277b23f1e2c1f64565fcb4f9fcd0780e
AnilaWaqar <waqar.sheikh28@gmail.com>
2024-03-21 19:58:17 -0700

    adding amazing CLI calculator Project

---
+++ app.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Printing welcome message:
+console.log("\n\tWelcome To \ 'Anila Waqar\' - CLI Amazing Calculator\n");
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Entersecond number", type: "number", name: "secondNumber" },
+    { message: "Select one of the operators to perform operation", 
+    type: "list",
+     name: "operator",
+     choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+    },
+]);
+console.log(answer);
+
+
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} 
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}  
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+} 
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("Please select opeation"); 
+}
+
